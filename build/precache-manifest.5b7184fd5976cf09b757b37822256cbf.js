7d44d9502a4751079d1a5e4f1fa5957229d57e0b
Yuliya <yulua03071992@gmail.com>
2022-03-28 14:51:12 +0300

    deploy

---
+++ build/precache-manifest.5b7184fd5976cf09b757b37822256cbf.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "586a5d25ad33b608e245c999b001ea8e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7f434e3a14480c8756de",
+    "url": "/static/css/2.ea48c431.chunk.css"
+  },
+  {
+    "revision": "9e27d48615ecf0b3b817",
+    "url": "/static/css/main.a0c3b36b.chunk.css"
+  },
+  {
+    "revision": "7f434e3a14480c8756de",
+    "url": "/static/js/2.fb9a6ad7.chunk.js"
+  },
+  {
+    "revision": "1972f29574012851c652",
+    "url": "/static/js/3.1749d0fc.chunk.js"
+  },
+  {
+    "revision": "9e27d48615ecf0b3b817",
+    "url": "/static/js/main.0904aaf0.chunk.js"
+  },
+  {
+    "revision": "cfc130b8b42e181a308e",
+    "url": "/static/js/runtime~main.6e2dc32a.js"
+  },
+  {
+    "revision": "8c10fa25aed7dd18ab4fa95662ad132c",
+    "url": "/static/media/Geometria.8c10fa25.ttf"
+  },
+  {
+    "revision": "0f6e082f42f5247d82c3ade9c2a8f928",
+    "url": "/static/media/Gilroy-ExtraBold.0f6e082f.otf"
+  }
+]);
